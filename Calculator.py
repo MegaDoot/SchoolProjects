1a7eb567dc44716311baa233813592c0f5572e25
MadLad <41120880+MegaDoot@users.noreply.github.com>
2019-09-23 15:18:54 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+#√
 
 FG = "black"
 BG = "white"
-FONT = "Consolas"
+FONT = "Calibri"
 BTN_STYLE = {"bg":BG, "fg":FG, "font":(FONT, 30)}
 ROWS = 4
 COLUMNS = 6
@@ -16,31 +17,80 @@ class App(tk.Tk):
         self.scale=1
         
         self.nums_frame = tk.Frame(self)
+        
 ##        self.nums_frame.grid_propagate(False)
 ##        for row in range(4):
 ##            self.nums_frame.grid_rowconfigure(row, weight=1)
 ##        for column in range(5):
 ##            self.nums_frame.grid_columnconfigure(column, weight=1)
-        self.add_btn("Ans", 0, 0, columnspan=2)        
+        self.screen_val = tk.StringVar(self, value = "")
+        self.screen = tk.Entry(self, state="normal", fg=FG, bg=BG, font=(FONT, 50), width = 0, textvariable=self.screen_val)
+        self.screen.bind("<Key>", lambda event: "break")
+        #width=0: width is governed by 'sticky' parameter in grid
+        
+
+        f = self.add_btn
+
+        f("√", 0, 0, op=True)
+        f("**", 0, 1, op=True)
+        f("%", 0, 2, op=True)
+        f("//", 0, 3, op=True)
+        f("CLR", 0, 4, command=self.screen_val.set(""))
+
+        f("*", 1, 3, op=True)
+        f("+", 2, 3, op=True)
+        f("(", 3, 3)
+        f("Ans", 4, 3)
+
+        f("/", 1, 4)
+        f("-", 2, 4)
+        f(")", 3, 4)
+        f("=", 4, 4)
+
         for n in range(1, 10):
             row = 3-((n-1)//3)
             column = (n-1)%3
             print(row, column)
-            self.add_btn(n, row, column)
- 
+            f(n, row, column)
 
-        self.nums_frame.grid(row=1, column=0, sticky="NESW")
+        f(0, 4, 0)
+        f(".", 4, 1)
+        f("DEL", 4, 2)
+        
+        #Place down everything
+        
+        self.screen.grid(row=1, column=0, sticky="NESW", pady=10)
+        self.nums_frame.grid(row=2, column=0, sticky="NESW")
         self.mainloop()
 
-    def add_btn(self, text, row, column, function=lambda *args: None, frame=None, rowspan=1, columnspan=1, **kwargs):
+    def get_curs(self): #Entry cursor position
+        return self.screen.index(tk.INSERT)
+    
+    def add_text(self, text):
+        print(self.get_curs())
+        current = self.screen_val.get()
+        current = list(current)
+        current.insert(self.get_curs()-1, text)
+        current = "".join(current)
+        self.screen_val.set(current)
+        print(current)
+
+    def error(self):
+        pass
+
+    def add_btn(self, text, row, column, command=None, frame=None, rowspan=1, columnspan=1, op=False):
         if frame is None:
             frame = self.nums_frame
+        print(text, command)
+        if command is None:
+            #print("command for '{}' is None".format(text))
+            command = lambda: self.add_text(text)
         frame = tk.Frame(master=self.nums_frame, height=BTN_SIZE * rowspan, width=BTN_SIZE * columnspan)
         frame.grid_propagate(False)
         frame.grid_rowconfigure(0, weight=1)
         frame.grid_columnconfigure(0, weight=1)
-        frame.grid(row=row, column=column, columnspan=columnspan, rowspan=rowspan, **kwargs)
-        tk.Button(frame, text=text, **BTN_STYLE).grid(row=0, column=0, sticky="NESW")
+        frame.grid(row=row, column=column, columnspan=columnspan, rowspan=rowspan)
+        tk.Button(frame, text=text, command=command, **BTN_STYLE).grid(row=0, column=0, sticky="NESW")
     
 if __name__ == "__main__":
     app = App()
