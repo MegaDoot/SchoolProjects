fba3cbcdf7ab84f1e357ef8d58f1917db94c1354
MadLad <41120880+MegaDoot@users.noreply.github.com>
2019-09-23 20:35:34 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -5,7 +5,7 @@ import math
 
 FG = "black"
 BG = "white"
-FONT = "Calibri"
+FONT = "Monospace"
 BTN_STYLE = {"bg":BG, "fg":FG, "font":(FONT, 30)}
 ROWS = 4
 COLUMNS = 6
@@ -13,11 +13,13 @@ BTN_HEIGHT = 60
 BTN_WIDTH = 100
 PADX = 3
 PADY = 5
+FUNCS = ("sqrt", "sin", "cos", "tan", "log")
 
 #For use in calculator
 class Functions:
     sqrt = math.sqrt
     tan = math.tan
+    log = math.log
 
     @staticmethod
     def sin(x):
@@ -25,38 +27,41 @@ class Functions:
 
     @staticmethod
     def cos(x):
-        return math.cos(math.radians(x)) 
+        return math.cos(math.radians(x))
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.answer = tk.Entry(fg=FG,bg=BG)
-        self.scale=1
-        
+        self.scale = 1
+        self.entry_frame = tk.Frame(self, height=110)
+        self.entry_frame.grid_columnconfigure(0, weight=1)
+        self.entry_frame.grid_propagate(False)
         self.nums_frame = tk.Frame(self)
         self.input_val = tk.StringVar(self, value = "")
-        self.input = tk.Entry(self, state="normal", fg=FG, bg=BG, font=(FONT, 30), width=0, textvariable=self.input_val, bd=0, highlightthickness=0)
+        self.input = tk.Entry(self.entry_frame, state="normal", fg=FG, bg=BG, font=(FONT, 30), width=0, textvariable=self.input_val, bd=0, highlightthickness=0)
         self.input.bind("<Key>", lambda event: "break")
+##        self.input_val.trace("w", lambda *args: self.set_result("", FG))
         self.input.focus_set()
-        
-        self.output = tk.Entry(self, state="disabled", disabledforeground=FG, disabledbackground=BG, font=(FONT, 40), width=0, bd=0, justify = "left")
+        self.output = tk.Entry(self.entry_frame, state="disabled", disabledforeground=FG, disabledbackground=BG, font=(FONT, 40), width=0, bd=0, justify = "right")
         #width=0: width is governed by 'sticky' parameter in grid
         
+        self.ans = 0
 
         f = self.add_btn
 
-        f("√", 0, 0, op=True)
-        f("**", 0, 1, op=True)
-        f("%", 0, 2, op=True)
-        f("//", 0, 3, op=True)
-        f("CLR", 0, 4, command=lambda: self.input_val.set(""))
+        f("√", 0, 0, operand=True)
+        f("**", 0, 1, operand=True)
+        f("%", 0, 2, operand=True)
+        f("//", 0, 3, operand=True)
+        f("CLR", 0, 4, command=lambda: (self.input_val.set(""), self.set_result("", FG)))
 
-        f("*", 1, 3, op=True)
-        f("+", 2, 3, op=True)
+        f("*", 1, 3, operand=True)
+        f("+", 2, 3, operand=True)
         f("(", 3, 3)
 
-        f("/", 1, 4)
-        f("-", 2, 4)
+        f("/", 1, 4, operand=True)
+        f("-", 2, 4, operand=True)
         f(")", 3, 4)
         
 
@@ -72,6 +77,8 @@ class App(tk.Tk):
         f("cos", 4, 3)
         f("tan", 4, 4)
 
+        f("log", 5, 0)
+        f(",", 5, 1)
         f("DEL", 5, 2, command=self.delete_char)
         f("Ans", 5, 3)
         f("=", 5, 4, command=self.evaluate)
@@ -79,6 +86,7 @@ class App(tk.Tk):
         
         self.input.grid(row=1, column=0, sticky="NESW")
         self.output.grid(row=2, column=0, sticky="NESW")
+        self.entry_frame.grid(row=0, column=0, sticky="NESW")
         self.nums_frame.grid(row=3, column=0, sticky="NESW")
         self.mainloop()
 
@@ -93,13 +101,18 @@ class App(tk.Tk):
             self.input.icursor(cursor_pos - 1)
             self.input_val.set("".join(current))
 
-    def add_text(self, text):
+    def add_text(self, text, operand=False):
+        if self.output.get() != "":
+            self.set_result("", FG)
+            if operand:
+                self.input_val.set("Ans")
+            else:
+                self.input_val.set("")
         func = False
-        if text == "√":
-            text = "√()"
+        if text in FUNCS or text == "√":
+            text += "()"
             func = True
         cursor_pos = self.get_curs()
-        print(cursor_pos)
         current = self.input_val.get()
         current = list(current)
         current.insert(cursor_pos, text)
@@ -109,32 +122,34 @@ class App(tk.Tk):
         if func:
             offset -= 1
         self.input.icursor(cursor_pos + offset)
-        print(current)
 
     def evaluate(self):
         text = self.input_val.get()
         text = text.replace("√", "sqrt")
-        print("text:", text)
+        for func in FUNCS:
+            text = text.replace(func, "Functions." + func)
+        text = text.replace("Ans", str(self.ans))
         try:
             result = eval(text)
             self.set_result(result, FG)
+            self.ans = result
         except Exception as error:
             self.set_result(type(error).__name__, "red")
     
     def set_result(self, text, colour):
         self.output.config(state="normal")
-        self.output.config(fg=colour)
+        self.output.config(disabledforeground=colour)
         self.output.delete(0, "end")
         self.output.insert(0, text)
         self.output.config(state="disabled")
 
-    def add_btn(self, text, row, column, command=None, frame=None, rowspan=1, columnspan=1, op=False):
+    def add_btn(self, text, row, column, command=None, frame=None, rowspan=1, columnspan=1, operand=False):
         if frame is None:
             frame = self.nums_frame
         print(text, command)
         if command is None:
             #print("command for '{}' is None".format(text))
-            command = lambda: self.add_text(text)
+            command = lambda: self.add_text(text, operand)
         frame = tk.Frame(master=self.nums_frame, height=BTN_HEIGHT * rowspan, width=BTN_WIDTH * columnspan)
         frame.grid_propagate(False)
         frame.grid_rowconfigure(0, weight=1)
