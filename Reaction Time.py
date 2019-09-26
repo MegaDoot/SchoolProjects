845b10e8fb46b89adf1506d827cb44f15c76c4d4
Alex Scorza <alex.scorza03@outlook.com>
2019-09-26 16:56:25 +0100

    Added graph (WIP)

--- Reaction Time.py
+++ Reaction Time.py
@@ -2,10 +2,15 @@ import tkinter as tk
 import turtle
 import time
 from random import randrange
+import threading
 
 FONT = "Consolas"
 FG = "black"
 BG = "white"
+CANVAS_WIDTH = 500
+CANVAS_HEIGHT = 200
+CANVAS_PADX = 10
+CANVAS_PADY = 10
 
 
 def get_time():
@@ -52,18 +57,25 @@ def add_scores(scores):
 ##        file.write(current)
 
 class Graph(tk.Frame):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, master):
+        super().__init__(master=master)
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
-        self.canvas = tk.Canvas(self)
+        self.canvas = tk.Canvas(self, height=CANVAS_HEIGHT, width=CANVAS_WIDTH)
         self.canvas.grid(row=0, column=0, sticky="NESW")
         self.t_screen = turtle.TurtleScreen(self.canvas)
         self.t_screen.delay(0)
-        self.t = turtle.RawTurtle(self.t_screen)
-        self.t.setpos(100, 100)
-        self.canvas.addtag_all("all")
+        self.line_t = turtle.RawTurtle(self.t_screen)
+        self.line_t.setpos(100, 100)
+        self.scale_t = turtle.RawTurtle(self.t_screen)
+##        self.scale_t.pu()
+        self.draw_all()
 
+    def draw_all(self):
+        flattened = sorted(flatten(get_scores()))
+        borders = (min(flattened), max(flattened))
+        self.scale_t.setpos(CANVAS_PADX - (CANVAS_WIDTH / 2), CANVAS_PADY - (CANVAS_HEIGHT / 2))
+        self.scale_t.write(str(borders[0]), align="center", font=(FONT, 20))
 
 class FrameLbl(tk.Frame):
     def __init__(self, master, width, height, **kwargs):
@@ -83,8 +95,8 @@ class Table(tk.Frame):
         self.columns = columns
         self.array = [
             [FrameLbl(
-                self, 180, 50, text="", relief="solid",
-                borderwidth=3, **style(15)) for _ in range(columns)
+                self, 160, 30, text="", relief="solid",
+                borderwidth=2, **style(15)) for _ in range(columns)
                 ] for _ in range(rows)
                 ]
         for row in range(rows):
@@ -122,19 +134,23 @@ class MainFrame(tk.Frame):
         super().__init__(master=master, **kwargs)
         self.master = master
         
-        tk.Label(self, text="Test your reaction times!", **style(50)).grid(
+        tk.Label(self, text="Test your reaction times!", **style(40)).grid(
             row=0, column=0)
         tk.Message(self, text="""Press begin and press any key as soon as you
 see the rectangle change colour. It will change randomly a total of 5 times
 and only your scores will be recorded for others to compete against!""",
-                   width=1500, justify="center", **style(20)).grid(
+                   width=1500, justify="center", **style(15)).grid(
                        row=1, column=0)
-        self.start_btn = tk.Button(self, text="Begin!", state="normal", command=self.start, **style(30))
+        self.start_btn = tk.Button(self, text="Begin!", state="normal", command=self.start, **style(25))
         self.start_btn.grid(row=2, column=0, pady=10)
-        self.colour = tk.Frame(self, bg="red", width=200, height=200)
+        self.colour = tk.Frame(self, bg="red", width=500, height=100)
         self.colour.grid(row=3, column=0, pady=10)
         self.info_lbl = tk.Label(self, text="", **style(20))
         self.info_lbl.grid(row=4, column=0)
+        self.graph_btn = tk.Button(self, text="View Graph", command=self.to_graph, **style(20))
+        self.graph_btn.grid(row=6, column=0)
+
+        self.graph = Graph(self)
         
         self.table = Table(self, 3, 7)
 
@@ -144,18 +160,29 @@ and only your scores will be recorded for others to compete against!""",
         self.table[1, 0] = "Your time (ms)"
         self.table[2, 0] = "Average (ms)"
         self.reset_table_times()
-        self.table.grid(row=6, column=0)
+        self.table.grid(row=7, column=0)
+        
         self.has_pressed = tk.BooleanVar(self, value=False)
-        self.master.bind("<Key>", self.key_pressed)
+        self.master.bind("<Key>", lambda event : threading.Thread(target=self.key_pressed).start())
 
 
-    def key_pressed(self, event):
+    def key_pressed(self):
+        self.update_idletasks()
+        print(self.colour.cget("bg"))
         if self.colour.cget("bg") == "green":
             self.has_pressed.set(True)
+        else:
+            print("penalty")
+            self.additional_time += 0.5
 
+    def to_graph(self):
+        self.table.grid_remove()
+        self.graph.grid(row=7, column=0)
 
     def one_loop(self, loop):
-##        time.sleep(randrange(20, 40) / 10)
+        print(self.additional_time)
+        time.sleep(self.additional_time)
+        self.additional_time = 0
         self.start_time = get_time()
         self.colour.config(bg="green")
         self.update()
@@ -188,6 +215,7 @@ and only your scores will be recorded for others to compete against!""",
         self.update_idletasks()
         
         time.sleep(2)
+        self.additional_time = 0
         self.info_lbl.config(text="Press any key as soon as it turns green!")
         for n in range(4):
             self.master.after(randrange(2000, 4000), self.one_loop(n))
